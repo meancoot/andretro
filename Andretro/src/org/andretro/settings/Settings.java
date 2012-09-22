a9a946b47eca6efbc45c45e1f133beb2161deb5f
jason <nowhere>
2012-09-22 19:37:34 -0400

    Readd fast forward/rewind button settings

--- Andretro/src/org/andretro/settings/Settings.java
+++ Andretro/src/org/andretro/settings/Settings.java
@@ -1,81 +1,13 @@
 package org.andretro.settings;
 
-import org.andretro.*;
 import org.andretro.emulator.*;
 
-import android.annotation.*;
 import android.content.*;
-import android.os.*;
 import android.preference.*;
-import android.view.*;
-import android.widget.*;
 
 final class Settings
 {
-	public static class GenericButton extends DialogPreference
-	{	
-		@TargetApi(12) public GenericButton(Context aContext, String aKey, String aName, int aDefault)
-		{
-			super(aContext, null);
-
-			setKey(aKey);
-			setTitle(aName);
-			setPersistent(true);
-			setDefaultValue(aDefault);
-			
-			// HACK: Set a layout that forces the dialog to get key focus
-			// TODO: Make the layout better looking!
-			setDialogLayoutResource(R.layout.dialog_focus_hack);
-			
-			setDialogTitle("Waiting for input");
-			setDialogMessage(aName);
-		}
-		
-		@Override protected void onAttachedToActivity()
-		{
-			super.onAttachedToActivity();
-			refreshSummary();
-		}
-		
-		@Override protected void showDialog(Bundle aState)
-		{
-			super.showDialog(aState);
-		
-			// HACK: Set the message in the hacked layout
-			((EditText)getDialog().findViewById(R.id.hack_message)).setText(getDialogMessage());
-			
-			getDialog().setOnKeyListener(new DialogInterface.OnKeyListener()
-			{	
-				@Override public boolean onKey(DialogInterface aDialog, int aKeyCode, KeyEvent aEvent)
-				{
-					persistInt(aKeyCode);
-					valueChanged(aKeyCode);
-					refreshSummary();					
-					getDialog().dismiss();
-					return false;
-				}
-			});
-		}
-		
-		@TargetApi(12) private void refreshSummary()
-		{
-	        if(android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.HONEYCOMB_MR1)
-	        {
-	        	setSummary(KeyEvent.keyCodeToString(getPersistedInt(0)));
-	        }
-	        else
-	        {
-	        	setSummary(Integer.toString(getPersistedInt(0)));
-	        }			
-		}
-		
-		protected void valueChanged(int aKeyCode)
-		{
-			
-		}
-	}
-
-	public static class Button extends GenericButton
+	public static class Button extends ButtonSetting
 	{
 		final Doodads.Button button;
 		
