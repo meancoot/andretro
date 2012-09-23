844f77f5b1b7b77cce046196eef825c8f6adfc30
jason <nowhere>
2012-09-22 22:45:55 -0400

    Move the Button setting out of Settings.java into InputActivity.java. Delete the unused Port setting, bring back when actually needed. Delete Settings.java cause it's empty now

--- Andretro/src/org/andretro/settings/InputActivity.java
+++ Andretro/src/org/andretro/settings/InputActivity.java
@@ -3,6 +3,7 @@ package org.andretro.settings;
 import org.andretro.emulator.*;
 
 import android.preference.*;
+import android.content.*;
 import android.os.*;
 
 @SuppressWarnings("deprecation")
@@ -21,7 +22,7 @@ public class InputActivity extends PreferenceActivity
 		Doodads.Device device = Game.getInputs().getDevice(0,  0);
 		for(Doodads.Button i: device.getAll())
 		{
-			screen.addPreference(new Settings.Button(this, i));
+			screen.addPreference(new Button(this, i));
 		}
 		
 		setPreferenceScreen(screen);
@@ -32,5 +33,22 @@ public class InputActivity extends PreferenceActivity
 		super.onPause();
 
 		Game.queueCommand(new Commands.RefreshInput(null));
+	}
+	
+	private static class Button extends ButtonSetting
+	{
+		final Doodads.Button button;
+		
+		public Button(Context aContext, final Doodads.Button aButton)
+		{
+			super(aContext, aButton.configKey, aButton.fullName, 0);
+			
+			button = aButton;
+		}
+				
+		@Override protected void valueChanged(int aKeyCode)
+		{
+			button.setKeyCode(aKeyCode);
+		}
 	}
 }
